61473e278cfc65acf8d068b2bdd640b9a32d53f6
Pushpendra Pal <94526347+Pushpendra100@users.noreply.github.com>
2022-09-29 22:29:45 +0530

    finally completed

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use("/api/polls", routes.poll)
 
 app.use(express.static(path.join(__dirname,"../client/build")));
 
-app.get("/",(req, res)=>{
+app.get("*",(req, res)=>{
     res.sendFile(path.resolve(__dirname,"../client/build/index.html"));
 });
 
